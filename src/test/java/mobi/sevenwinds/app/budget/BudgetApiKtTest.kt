058e19fc5a35bdacc152f109c8852af12c7aceb9
Ivan <clicker.heroes.acg@gmail.com>
2023-03-20 14:49:52 +0300

    config fix, right assertions

--- src/test/java/mobi/sevenwinds/app/budget/BudgetApiKtTest.kt
+++ src/test/java/mobi/sevenwinds/app/budget/BudgetApiKtTest.kt
@@ -6,7 +6,7 @@ import mobi.sevenwinds.common.jsonBody
 import mobi.sevenwinds.common.toResponse
 import org.jetbrains.exposed.sql.deleteAll
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.junit.Assert
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -32,10 +32,12 @@ class BudgetApiKtTest : ServerTest() {
             .get("/budget/year/2020/stats")
             .toResponse<BudgetYearStatsResponse>().let { response ->
                 println("${response.total} / ${response.items} / ${response.totalByType}")
-
-                Assert.assertEquals(5, response.total)
-                Assert.assertEquals(3, response.items.size)
-                Assert.assertEquals(105, response.totalByType[BudgetType.Приход.name])
+                Assertions.assertEquals(5, response.total)
+                Assertions.assertEquals(3, response.items.size)
+                Assertions.assertEquals(
+                    105,
+                    response.totalByType[BudgetType.Приход.name]
+                )
             }
     }
 
@@ -54,11 +56,11 @@ class BudgetApiKtTest : ServerTest() {
             .toResponse<BudgetYearStatsResponse>().let { response ->
                 println(response.items)
 
-                Assert.assertEquals(30, response.items[0].amount)
-                Assert.assertEquals(5, response.items[1].amount)
-                Assert.assertEquals(400, response.items[2].amount)
-                Assert.assertEquals(100, response.items[3].amount)
-                Assert.assertEquals(50, response.items[4].amount)
+                Assertions.assertEquals(30, response.items[0].amount)
+                Assertions.assertEquals(5, response.items[1].amount)
+                Assertions.assertEquals(400, response.items[2].amount)
+                Assertions.assertEquals(100, response.items[3].amount)
+                Assertions.assertEquals(50, response.items[4].amount)
             }
     }
 
@@ -80,7 +82,7 @@ class BudgetApiKtTest : ServerTest() {
             .jsonBody(record)
             .post("/budget/add")
             .toResponse<BudgetRecord>().let { response ->
-                Assert.assertEquals(record, response)
+                Assertions.assertEquals(record, response)
             }
     }
 }
